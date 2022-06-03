0f7bd754584af53485381a58b7e856de854e0a45
egor skrypnikov <eskrypn@gmail.com>
2022-06-03 13:22:23 +0300

    Cleanupped

--- treesitter_stage.py
+++ treesitter_stage.py
@@ -14,6 +14,8 @@ def load_treesitter(languages: List[str]) -> Dict[str, Language]:
 
     monikers = list(map(lambda s: f'tree-sitter-{s.lower()}', languages))
 
+    # How threadsafe is cloning?
+
     for ts_moniker in monikers:
         if not path.exists(f'build/{ts_moniker}'):
             exit_code = system(f'git clone https://github.com/tree-sitter/{ts_moniker} build/{ts_moniker}')
