b4ee6da7ae8e6e8c1e267d98ab7862ed02738f42
egor skrypnikov <eskrypn@gmail.com>
2022-06-15 01:04:32 +0300

    First draft of data processing

--- repoparse/analysis.py
+++ repoparse/analysis.py
@@ -1,6 +1,7 @@
 import json
 from sklearn.neighbors import KDTree
 from os import walk
+from typing import Dict, List
 
 
 def read_data(base_path: str = "./out", data_file_prefix: str = "repoparse_"):
@@ -22,24 +23,71 @@ def read_data(base_path: str = "./out", data_file_prefix: str = "repoparse_"):
 
     return merged_dict
 
+
+def make_trigrams_cnt(identifier: str) -> Dict[str, int]:
+    res = {}
+    for trigram in map(lambda tup: ''.join(tup), zip(*[identifier[i:] for i in range(3)])):
+        if trigram not in res:
+            res[trigram] = 1
+        else:
+            res[trigram] += 1
+    return res
+
+# TODO generics
+def merge_cnts(left_dict, right_dict) -> dict:
+    result = right_dict.copy()
+    for k, v in left_dict:
+        if k in result:
+            result[k] += v
+        else:
+            result[k] = v
+
+
 def proces_data(merged_dict): # The typing of this thing is too cumbersome
+    # An object that maps the author into two sparse matrices that are
+    # a) the amounts of lines by-language
+    # b) the trigram counts
+    final_author_dict: Dict[str, Dict[str, int]] = {}
+
     for _, repo_dict in merged_dict.items():
-        for _, author_dict in repo_dict.items():
+        for author_name, author_dict in repo_dict.items():
+            languages_subdict = {}
+            trigrams_subdict = {}
+
             for _, commit_dict in repo_dict.items():
                 # Nonempty lines amount
                 patch_set = commit_dict["patch_set"]
                 for k, v in patch_set.items():
                     patch_set[k] = len(list(filter(lambda x: x != "", v.split("\n"))))
+
+                patch_languages = commit_dict["patch_languages"]
+                # Slap those together
+                for k, v in patch_languages:
+                    if v not in languages_subdict:
+                        languages_subdict[v] = patch_set[k]
+                    else:
+                        languages_subdict[v] += patch_set[k]
+
                 patch_ids = commit_dict["patch_ids"]
 
-                trigrams_dict = {}
-                # for k, v in patch_ids.items():
-                # make trigrams and map them to the trigram numbers
+                for k, v in patch_ids.items():
+                    for identifier in v:
+                        trigrams_subdict = trigrams_subdict.union(make_trigrams_cnt(identifier))
+
                 # make sparse matrices
                 # load 2 matrices (by-language linenums + trigrams)
                 # into two kdtrees
                 # combine scores for queries
 
-
-
+            if author_name not in final_author_dict:
+                final_author_dict[author_dict] = {
+                    "languages": languages_subdict,
+                    "trigrams": trigrams_subdict
+                }
+            else:
+                final_author_dict[author_dict] = {
+                    "languages": merge_cnts(final_author_dict["languages"], languages_subdict),
+                    "trigrams": merge_cnts(final_author_dict["trigrams"], trigrams_subdict)
+                }
+# WRITE A QUERY REPL
 
